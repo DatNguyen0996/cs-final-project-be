0a77129c648bbb0e54dbb5480461a2c8bd8edcb2
Dat_Nguyen <ngxudat@gmail.com>
2023-03-27 10:37:44 +0700

    fix cors

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
